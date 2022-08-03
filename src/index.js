2b9a668d012a7260cdbe1ae9db4d4ec91af9be5d
Sahil Kumar <77158996+sahilkhatkar@users.noreply.github.com>
2022-08-03 11:15:41 +0530

    2nd commit

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
-    // domain={'dev-apvkndwn.us.auth0.com'}
-    // clientId={'5Fj4bgY6MnQEw9hSg6wiyW5I1wHQzV1f'}
-    domain={domain}
-    clientId={clientId}
+    domain={'dev-apvkndwn.us.auth0.com'}
+    clientId={'5Fj4bgY6MnQEw9hSg6wiyW5I1wHQzV1f'}
+    // domain={domain}
+    // clientId={clientId}
     redirectUri={window.location.origin}>
     <App />
   </Auth0Provider>
